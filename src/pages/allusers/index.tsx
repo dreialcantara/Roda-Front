0cb473522e3a3ebced28080821e054143f029dd2
dreialcantara <andreialcantara@id.uff.br>
2022-09-29 15:29:32 -0300

    redux adjust

--- src/pages/allusers/index.tsx
+++ src/pages/allusers/index.tsx
@@ -2,9 +2,12 @@ import Button from "react-bootstrap/Button";
 import { useSelector } from "react-redux";
 import { ListarUsuario } from "../../services/MainApi/listarusuarios";
 import { RootStore } from "../../store";
+import { removeUser } from "../../store/modules/user";
+import { useDispatch } from "react-redux";
 
 function AllUsers() {
   const user = useSelector((store: RootStore) => store.userReduce);
+  const dispatch = useDispatch();
   const listar = async (event: any) => {
     event.preventDefault();
 
@@ -26,6 +29,9 @@ function AllUsers() {
       <h1>O e-mail logado é o {user.email}</h1>
       <Button variant="success" onClick={listar}>
         Listar Usuários
+      </Button>
+      <Button variant="danger" onClick={dispatch(removeUser)}>
+        Sair
       </Button>
     </>
   );
